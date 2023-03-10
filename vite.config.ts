e0d3e4d971419601a98c4ef36dee13f798ac9e14
htx <huangtingxin@rayvison.com>
2023-03-10 10:20:04 +0800

    ✨ 支持对低版本浏览器的兼容

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    legacy({
+      targets: ['defaults'],
+    }),
+  ],
   resolve: {
     alias: [
       {
@@ -27,7 +35,7 @@ export default defineConfig({
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    target: 'es2015',
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
